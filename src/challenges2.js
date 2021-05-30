81131d53cc4ac7483e53f2cfff6df1d52e834440
Thaís Sampaio <thais_castrocs@outlook.com>
2021-05-30 16:54:42 -0300

    Desafio10 realizado sem erro do lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,17 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let listaTecnologia = [];
+  let tecnologiaOrdenadas = tech.sort();
+
+  if (tech.length === 0) {
+    return 'Vazio!';
+  }
+  for (let index = 0; index < tech.length; index += 1) {
+    listaTecnologia.push({ tech: tecnologiaOrdenadas[index], name });
+  }
+  return listaTecnologia;
 }
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
